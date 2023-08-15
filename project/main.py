cdab8eb5e04669a59738c608f0097408d1324522
BilalLaaroussiElAlami <bilal.laaroussi.el.alami@vub.be>
2023-08-15 16:34:57 +0200

    allow to change axises

--- project/main.py
+++ project/main.py
@@ -1,26 +1,57 @@
 import pandas as pd
-from bokeh.io import show
-from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.io import show, curdoc
+from bokeh.layouts import column, row
+from bokeh.models import ColumnDataSource, HoverTool, Select
 from bokeh.plotting import figure
 
-# p = figure(title="Multiple glyphs example", x_axis_label="x", y_axis_label="y")
-# x = [1, 2, 3]
-# y = [3,2,1]
-# p.circle(x, y, legend_label="Objects", color="blue", size=12)
-# show(p)
 
 
 # maka a dataframe from the csv file
-df = pd.read_csv('project/smartphones.csv')
+df = pd.read_csv('smartphones.csv')
 # print the brands of the phones
-print(df['brand_name'].unique())
+#print(df['brand_name'].unique())
 
+"""
+axis_map = {
+    "Tomato Meter": "Meter",
+    "Numeric Rating": "numericRating",
+    "Number of Reviews": "Reviews",
+    "Box Office (dollars)": "BoxOffice",
+    "Length (minutes)": "Runtime",
+    "Year": "Year",
+}"""
+
+attribute_map = {
+    "Brand": "brand_name",
+    "Model": "model",
+    "Price": "price",
+    "Average Rating": "avg_rating",
+    "5G Support": "5G_or_not",
+    "Processor Brand": "processor_brand",
+    "Number of Cores": "num_cores",
+    "Processor Speed": "processor_speed",
+    "Battery Capacity": "battery_capacity",
+    "Fast Charging Available": "fast_charging_available",
+    "Fast Charging": "fast_charging",
+    "RAM Capacity": "ram_capacity",
+    "Internal Memory": "internal_memory",
+    "Screen Size": "screen_size",
+    "Refresh Rate": "refresh_rate",
+    "Number of Rear Cameras": "num_rear_cameras",
+    "Operating System": "os",
+    "Primary Rear Camera": "primary_camera_rear",
+    "Primary Front Camera": "primary_camera_front",
+    "Extended Memory Available": "extended_memory_available",
+    "Resolution Height": "resolution_height",
+    "Resolution Width": "resolution_width",
+}
+
+
+x_axis = Select(title="X Axis", options=sorted(attribute_map.keys()), value="Battery Capacity")
+y_axis = Select(title="Y Axis", options=sorted(attribute_map.keys()), value="Price")
 
 # make a graph where the x-axis is the battery_capacity and the y-axis is the price, plot the phone models as circles, when the user hovers over a circle
 # more information is shown about the phone model:
-#p = figure(title="Battery capacity vs price", x_axis_label="Battery capacity", y_axis_label="Price")
-#p.circle(df['battery_capacity'], df['price'], size=10)
-#show(p)
 
 # Create a ColumnDataSource for the data
 source = ColumnDataSource(data=dict(
@@ -40,5 +71,26 @@ hover = HoverTool()
 hover.tooltips = [("Model", "@model")]
 p.add_tools(hover)
 
-# Show the plot
-show(p)
+
+def update():
+    print("updated")
+
+    x_name = attribute_map[x_axis.value]
+    y_name = attribute_map[y_axis.value]
+
+    p.xaxis.axis_label = x_axis.value
+    p.yaxis.axis_label = y_axis.value
+    p.title.text = f"{len(df)} movies selected"
+    source.data = dict(
+        x=df[x_name],
+        y=df[y_name],
+        model=df['model']
+    )
+
+controls = [ x_axis, y_axis]
+for control in controls:
+    control.on_change('value', lambda attr, old, new: update())
+
+layout =  row(column(x_axis,y_axis),p)
+#show(layout)
+curdoc().add_root(layout)
