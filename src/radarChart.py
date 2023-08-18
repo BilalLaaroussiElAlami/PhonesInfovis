10e53c8abe7f0d6a0b38fe07a0f8c06863473be6
BilalLaaroussiElAlami <bilal.laaroussi.el.alami@vub.be>
2023-08-18 02:18:50 +0200

    working on radarchart

--- src/radarChart.py
+++ src/radarChart.py
@@ -1,16 +1,55 @@
 #reference: https://vecco-insight.gitbook.io/crash-visualization/plotly/6.3-statistical-charts/6.3.3-radar-chart
 
+import plotly.express as px
 import pandas as pd
 
-df = pd.DataFrame(dict(
-    r=[4, 5, 2, 4, 3],
+
+
+def radarplots(valsA,nameA,valsB,nameB, labels):
+    df1 = pd.DataFrame(dict(
+        r=valsA,
+        theta=labels))
+    df2 = pd.DataFrame(dict(
+        r=valsB,
+        theta=labels))
+
+    fig = px.line_polar(df1, r='r', theta='theta', line_close=True)
+    fig.update_traces(fill='toself', name=nameA)
+
+    fig.add_trace(
+        px.line_polar(df2, r='r', theta='theta', line_close=True)
+        .update_traces(fill='toself', name=nameB)
+        .data[0]
+    )
+    return fig
+
+
+#example usage of function radarplots
+X =  radarplots([0.2, 0.8, 0.4, 0.3, 1], "YUU", [0.5, 0.6, 0.9, 0.7, 0.5], "YUUKES", ['Difficulty', 'Execution', 'Landing','Style', 'Creativity'])
+X.show()
+"""
+# Create multiple dataframes with different data
+df1 = pd.DataFrame(dict(
+    r=[0.2, 0.8, 0.4, 0.3, 1],
     theta=['Difficulty', 'Execution', 'Landing',
            'Style', 'Creativity']))
 
-import plotly.express as px
+df2 = pd.DataFrame(dict(
+    r=[0.5, 0.6, 0.9, 0.7, 0.5],
+    theta=['Difficulty', 'Execution', 'Landing',
+           'Style', 'Creativity']))
 
+# Create a figure and add multiple polar line traces
+fig = px.line_polar(df1, r='r', theta='theta', line_close=True)
+fig.update_traces(fill='toself', name='Dataset 1')
 
+fig.add_trace(
+    px.line_polar(df2, r='r', theta='theta', line_close=True)
+    .update_traces(fill='toself', name='Dataset 2')
+    .data[0]
+)
 
-fig = px.line_polar(df, r='r', theta='theta', line_close=True)
-fig.update_traces(fill='toself')
+# You can add more traces similarly for more datasets
+
 fig.show()
+"""
